05f9cdbe6f3238d15ac9c8d002a74b229c1681cc
Ayushkai001 <kmarayush28@gmail.com>
2026-08-28 05:35:44 +0530

    Revision

--- Rag_Project/main.py
+++ Rag_Project/main.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI # type: ignore
 load_dotenv()
 
-mistral = ChatMistralAI.from_env()
+model = ChatMistralAI(model_name="mistral-small-2506")
